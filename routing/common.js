a8d86d6b2841c97cb4b0da444884ba1a23ef6ac5
Robert Mrowiec <rmrowiec@icloud.com>
2018-07-31 19:24:41 +0200

    update multifilter function

--- routing/common.js
+++ routing/common.js
@@ -8,6 +8,14 @@ function defaultResponse(func) {
 module.exports.multiFilter = multiFilter
 function multiFilter(body) {
   let result = Object.assign({}, body)
+  if (body.startMonth >= 0 && !body.endMonth){
+    result.date = {$gt: new Date(2018, Number(body.startMonth), 1)}
+    delete result.startMonth
+  }
+  if (!body.startMonth && body.endMonth){
+    result.date = {$lt: new Date(2018, body.endMonth + 1, 1)}
+    delete result.endMonth
+  }
   if (body.startMonth >= 0 && body.endMonth){
     result.date = {$gt: new Date(2018, Number(body.startMonth), 1), $lt: new Date(2018, body.endMonth + 1, 1)}
     delete result.startMonth
